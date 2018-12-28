4131b2d52cbcdf7df6375a081eb2958812bf3cad
anansasdsa <41487794+anansasdsa@users.noreply.github.com>
2018-12-28 04:26:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ fetchAllMembers: true });
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 const fs = require("fs");
 const Canvas = require('canvas')
 const jimp = require('jimp')
@@ -50,7 +51,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
     }
 });
 
@@ -68,7 +69,151 @@ client2.on('message', message => {
 
 client2.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#rep')
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client4.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client4.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client4.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#rep @!         iTzAnan_Over#3675')
     }
 });
 
@@ -101,5 +246,126 @@ if (message.content === '*spam') {
         }
       }
 });
+
+client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client5.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client6.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client7.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client8.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client9.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client10.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
 client.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN2);
+client3.login(proccess.env.BOT_TOKEN3);
+client4.login(proccess.env.BOT_TOKEN4);
+client5.login(proccess.env.BOT_TOKEN5);
+client6.login(proccess.env.BOT_TOKEN6);
+client7.login(proccess.env.BOT_TOKEN7);
+client8.login(proccess.env.BOT_TOKEN8);
+client9.login(proccess.env.BOT_TOKEN9);
+client10.login(prccess.env.BOT_TOKEN10);
